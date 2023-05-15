2df09fc666e3719f1e02d2e236bdb6979257f0cb
ibBenwah <garret.c.benoit@gmail.com>
2023-05-15 00:08:50 -0400

    Exponents Lab practice

---
+++ Exponents Lab1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Net.Http.Headers;
+
+bool goAgain = true;
+
+do
+{
+    begin:  Console.Write("Please enter an integer: ");
+
+    
+    double input = Convert.ToDouble(Console.ReadLine());
+    double squared = Math.Pow(input, 2);
+    double cubed = input * input * input;
+
+    Console.WriteLine($"Number          Squared        Cubed ");
+    Console.WriteLine($"=====           =======        ======");
+    
+    Console.WriteLine(string.Format($"{input, -15} {squared, -15} {cubed, -25}"));
+
+    Console.Write("Keep Going? Enter (y/n) ");
+     string keepGoing = Console.ReadLine().ToLower();
+    if (keepGoing == "y")
+    {
+        goto begin;
+    }
+    goAgain = false;
+    
+}while(goAgain);
